fb5bbf8330dcb8f74590346b1a3ac71af5019354
Haley Alexander <hal.00.alexander@gmail.com>
2016-04-21 12:39:00 -0500

    Create Hello.py

---
+++ Hello.py
@@ -0,0 +1,14 @@
+#start program 
+def main(): 
+  # # is a comment, nothing happens on lines starting with #
+  #white space matters
+  #so make sure to indent 
+  
+  #this is how you print
+  print("Hello World") 
+  
+#ending program 
+main() 
+
+#now try to print your name 
+  
