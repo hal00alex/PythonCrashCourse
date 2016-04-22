6cb5a97301dc42a42ba7a6869a93e2ead3091024
Haley Alexander <hal.00.alexander@gmail.com>
2016-04-22 11:53:21 -0500

    Create If.py

---
+++ If.py
@@ -0,0 +1,13 @@
+def main(): 
+
+  x = 10 
+  #the colon and indent is important 
+  if (x < 5): 
+    print ("X is less than 5") 
+  
+  Else: 
+    print ("X is 5 or greater") 
+
+  #comparisons: and, or, ==, !=, <, >, <=, >=
+  #now try changing X or the comparison 
+main()
